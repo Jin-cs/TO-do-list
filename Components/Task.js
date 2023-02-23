d60b485b39957e248c4a503c02c9ba1ed6260392
Jin-cs <77925934+Jin-cs@users.noreply.github.com>
2023-02-23 21:29:59 +0700

    Add files via upload

--- Components/Task.js
+++ Components/Task.js
@@ -3,34 +3,70 @@ import { StyleSheet, Text, View} from 'react-native';
 import { useFonts } from "expo-font";
 import { useState } from "react";
 import Checkbox from "expo-checkbox";
+import { db } from "../Database/Firebase";
+import { doc, deleteDoc} from "firebase/firestore";
+
+
+
 
 /* สีต่างๆ */
 const textcolor = '#1B1C1E';
 const light_grey = '#f0f1f3';
 const dark_grey = '#CDD1D9';
-const light_yellow = '#F7D28C';
-const light_green = '#39DB8D';
-const light_pink = '#F65279';
-const light_blue = '#5BA9F0';
+const light_yellow = '#ffe084';
+const light_green = '#74e5af';
+const light_pink = '#f997ae';
+const light_blue = '#8cc2f4';
+
+
 
 const Task = (props) => {
     const [isChecked, setChecked] = useState(false);
+    const colorSelected = props.color;
+
+    //const db = getFirestore();
+
+    // const docRef = doc(db, "Task", "");
+   
+
+    // deleteDoc(docRef)
+    //  .then(() => {
+    //     console.log("Entire Document has been deleted successfully.")
+    // })
+    // .catch(error => {
+    //     console.log(error);
+    // })
+    const [refreshing, setRefreshing] = React.useState(false);
+
+    const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+        setRefreshing(false);
+    }, 2000);
+    }, []);
+
+    
     return(
-        <View style={styles.box}>
-            <Checkbox style={styles.checkbox} value={isChecked} 
-                onValueChange={setChecked} color={isChecked ? /*เปลี่ยนสีตอน check*/dark_grey : undefined}/>
+        <View style={[styles.box, {backgroundColor: colorSelected, borderRadius: 20}]}>
+            <Checkbox style={styles.checkbox} value={isChecked} onTouchEnd = {async()=>{
+                console.log(props.id);
+                await deleteDoc(doc(db, "Task", props.id));   
+            }} 
+                onValueChange={setChecked} color={isChecked ? /*เปลี่ยนสีตอน check*/textcolor : undefined}
+                />
             <Text style={styles.taskText}>{props.text}</Text>
-            <Text style={styles.dateText}>31/01/23</Text>
+            <Text style={styles.dateText}>{props.date}</Text>
         </View>
     )
 }
+
 
 const styles = StyleSheet.create({
     dateText: {
         position: 'absolute',
         marginLeft: '23%',
         bottom: '30%',
-        opacity: 0.4,
+        opacity: 0.5,
         fontFamily: 'PoppinsRegular'
     },
     
@@ -40,7 +76,7 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         borderRadius: 15,
         marginTop: '3%',
-        borderColor: dark_grey,
+        borderColor: textcolor,
 
     },
     
@@ -56,7 +92,7 @@ const styles = StyleSheet.create({
     
     box: {
         /* เลือกสีกล่อง */
-        backgroundColor: light_grey,
+        
 
         width: '80%',
         height: '15%',
