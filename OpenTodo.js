9c2457717cc056916ae5fdb0e6efc9f030b04228
saranya-sinseeda <79294193+saranya-sinseeda@users.noreply.github.com>
2023-05-02 23:44:09 +0700

    Update OpenTodo.js

--- OpenTodo.js
+++ OpenTodo.js
@@ -4,17 +4,28 @@ import { useFonts } from 'expo-font';
 import { useNavigation } from '@react-navigation/native';
 import Task from './Components/Task';
 import { TouchableOpacity } from 'react-native';
-import { useEffect } from 'react';
-import {useState} from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { collection, getDocs} from "firebase/firestore";
 import { db } from './Database/Firebase';
+import * as Notifications from "expo-notifications";
+import * as Device from "expo-device";
+import * as Permissions from "expo-permissions";
 
-
-
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
 
 export default  function OpenTodo() {
 
   const [todos, onChangeText] = useState([]);
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
 
   const fetchPost = async () => {
      
@@ -30,6 +41,20 @@ export default  function OpenTodo() {
  
   useEffect(()=>{
       fetchPost();
+      registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
   }, [])
 
   const [loaded] = useFonts({
@@ -127,3 +152,36 @@ const styles = StyleSheet.create({
 
 });
 
+
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  return token;
+}
